4c68d2238549d324440e1bad812b8287c4ad2212
Amirtharaj <amirtharaj146@gmail.com>
2023-08-22 06:35:35 -0700

    sum of c

--- ex1.java
+++ ex1.java
@@ -2,6 +2,8 @@ class ex1
 {
     public static void main(String a1[])
     {
-        System.out.println("hello");
+        int a=5,b=10,c;
+        c=a+b;
+        System.out.println("hello"+c);
     }
 }
