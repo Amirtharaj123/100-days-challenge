6e2e85eedfee6e2e8c3d783f2484e6f009ef97ea
Amirtharaj <amirtharaj146@gmail.com>
2023-08-23 05:34:09 -0700

    Right Angled triangle

---
+++ logic2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class logic
+{
+    public static void main(String a1[])
+    {
+        Scanner m=new Scanner(System.in);
+        System.out.print("Enter the limit:");
+        int n=m.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;i>=j;j++)
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
