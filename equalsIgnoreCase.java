3068655a6786faf5709f8903b1e595f329197f60
Amirtharaj <amirtharaj146@gmail.com>
2023-09-12 20:49:53 -0700

    the two string are equal to check boolean datatype using without case sensitive

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class simple
+{
+    public static void main(String a1[])
+    {
+        Scanner m=new Scanner(System.in);
+        System.out.print("Enter the String 1:");
+        String s=m.nextLine();
+        System.out.print("Enter the String 2:");
+        String s1=m.nextLine();
+        if(s.equalsIgnoreCase(s1)==true)
+          System.out.print("two string are equal");
+        else
+          System.out.print("two string are not equal");
+    }
+}
