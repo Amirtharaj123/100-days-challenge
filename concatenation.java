19a1caaaa34270da070eb078e7320e7d840f1c8f
Amirtharaj <amirtharaj146@gmail.com>
2023-09-13 21:28:28 -0700

    the two string concatenate to single word

---
+++ concatenation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class simple
+{
+    public static void main(String a1[])
+    {
+        Scanner m=new Scanner(System.in);
+        System.out.print("Enter the string 1:");
+        String s1=m.nextLine();
+        System.out.print("Enter the string 2:");
+        String s2=m.nextLine();
+        System.out.print(s1.concat(s2));
+    }
+}
+
