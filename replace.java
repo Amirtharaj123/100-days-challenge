360caaeda2e31fb1c39f665def867a48dd5effbd
Amirtharaj <amirtharaj146@gmail.com>
2023-09-13 21:22:54 -0700

    the string replace one word to another word

---
+++ replace.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class simple
+{
+    public static void main(String a1[])
+    {
+        Scanner m=new Scanner(System.in);
+        System.out.print("Enter the string 1:");
+        String s1=m.nextLine();
+        System.out.print(s1.replace("manibalan","amirtharaj"));
+    }
+}
+
