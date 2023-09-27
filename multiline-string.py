97d5a59078c4a42ccfd0b9edb0f8c693f8ca7efe
Amirtharaj <amirtharaj146@gmail.com>
2023-09-26 22:06:46 -0700

    the multi line string in single or double three quotes in python

---
+++ multiline-string.py
@@ -0,0 +1,9 @@
+a="""you can assign a multiline string to a variable by using three quotes
+    this is string multiple line using three double quotes used"""
+
+print(a)
+
+a='''you can assign a multiline string to a variable by using three quotes
+    this is string multiple line using three double quotes used'''
+
+print(a)
